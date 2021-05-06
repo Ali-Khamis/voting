0c098dba7df8a7248c37104603a640690824342b
Ali-Khamis <al.tramsia@gmail.com@gmail.com>
2021-05-06 17:47:18 +0200

    removed unneeded cdoe and files

--- components/SignOut.tsx
+++ components/SignOut.tsx
@@ -1,6 +1,4 @@
 import React, { forwardRef } from "react";
-import HandleSignOut from "../types";
-// import { useDispatch } from "react-redux";
 
 const SignOut = forwardRef(
   ({ onClick, href }: { onClick: any; href: string }, ref: any) => {
